e28114eac32e4666ecd1b32dedbc701e492d3d17
andrewjcatoiu <andrewjcatoiu26@gmail.com>
2025-01-04 15:58:50 -0600

    basic init outline

---
+++ Game/Board.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Board {
+
+    private final ArrayList<String> players;
+    private final ArrayList<String> tiles;
+    private final ArrayList<String> deck;
+
+    public Board(ArrayList<String> players, ArrayList<String> tiles, ArrayList<String> deck) {
+        this.players = new ArrayList<>();
+        this.tiles = tiles;
+        this.deck = deck;
+    }    
+}
