9ad99e8afcfbd340aa1d847deff29762e2519551
andrewjcatoiu <andrewjcatoiu26@gmail.com>
2025-01-12 15:26:59 -0600

    number getter for Tile

--- Game/Tile.java
+++ Game/Tile.java
@@ -8,15 +8,30 @@ public class Tile {
     public Tile(String m, int n, int x, int y) {
         material = m;
         number = n;
-        x = x;
-        y = y;
+        this.x = x;
+        this.y = y;
     }
 
     public Tile(String m) {
         material = m;
+        number = 0;
     }
 
     public String getMaterial() {
         return material;
     }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
 }
