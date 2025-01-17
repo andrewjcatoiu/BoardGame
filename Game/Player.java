86670a571cfa1ec54cc58943773d8d94a99a6895
andrewjcatoiu <andrewjcatoiu26@gmail.com>
2025-01-17 14:39:49 -0600

    Player docs

--- Game/Player.java
+++ Game/Player.java
@@ -2,16 +2,55 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class responsible for player moves.
+ * 
+ * @author Andrew Catoiu
+ * @version January 2025
+ */
 public class Player {
     
+    /**
+     * The name of the player.
+     */
     private String name;
+
+    /**
+     * The color structures the player will build.
+     */
     private String color;
+
+    /**
+     * The number of settlements the player can build.
+     */
     private int settlements;
+
+    /**
+     * The number of cities the player can build.
+     */
     private int cities;
+
+    /**
+     * The number of roads the player can build.
+     */
     private int roads;
+
+    /**
+     * The player's current hand of cards.
+     */
     private final Map<String, Integer> hand;
+
+    /**
+     * A map of tile vertex coordinates controlled by the player.
+     */
     private final Map<Integer, ArrayList<int[]>> activeCoords;
 
+    /**
+     * Default constructor.
+     * 
+     * @param name the player's name
+     * @param color the player's building color
+     */
     public Player(String name, String color) {
         this.name = name;
         this.color = color;
@@ -29,30 +68,69 @@ public class Player {
         this.activeCoords = new HashMap<>();
     }
 
+    /**
+     * Getter for player's name.
+     * 
+     * @return returns a string
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Getter for player's building color.
+     * 
+     * @return returns a string
+     */
     public String getColor() {
         return this.color;
     }
 
+    /**
+     * Getter for the number of settlements the player can build.
+     * 
+     * @return returns an integer
+     */
     public int getSettlements() {
         return this.settlements;
     }
 
+    /**
+     * Getter for the number of cities the player can build.
+     * 
+     * @return returns an integer
+     */
     public int getCities() {
         return this.cities;
     }
 
+    /**
+     * Getter for the number of roads the player can build.
+     * 
+     * @return returns an integer
+     */
     public int getRoads() {
         return this.roads;
     }
 
+    /**
+     * Getter for the player's controlled coordinates.
+     * 
+     * @return returns a map of tile entries with the tile index as key and list of coordinate pairs as value
+     */
     public Map<Integer, ArrayList<int[]>> getActiveCoords() {
         return this.activeCoords;
     }
 
+    /**
+     * Updates the player's controlled coordinates map after building.
+     * 
+     * @param tileIndex used to store the coordinate pair
+     * @param x the x-coordinate of the newly built structure
+     * @param y the y-coordinate of the newly built structure
+     * @param isSettlement flag to indicate if the structure built is a settlement
+     * @param isCity flag to indicate if the structure built is a city
+     */
     public void updateActiveCoords(int tileIndex, int x, int y, int isSettlement, int isCity) {
         ArrayList<int[]> tileCoords = this.activeCoords.get(tileIndex);
         if (tileCoords == null) {
@@ -64,12 +142,25 @@ public class Player {
         this.activeCoords.put(tileIndex, tileCoords);
     }
 
-    public void updateHand(String material, int num) {
+    /**
+     * Updates the current hand of the player.
+     * 
+     * @param material the material to update
+     * @param inc the amount to increment onto the material count
+     */
+    public void updateHand(String material, int inc) {
         int currAmount = this.hand.get(material);
-        currAmount += num;
+        currAmount += inc;
         this.hand.put(material, currAmount);
     }
 
+    /**
+     * Builds a settlement. Updates player's hand. Updates active coordinates map, with new tiles and coordinates to be 
+     * controlled. Sets settlement flag to true.
+     * 
+     * @param x the x-coordinate of the tile vertex
+     * @param y the y-coordinate of the tile vertex
+     */
     public void buildSettlement(int x, int y) {
         updateHand("Wood", -1);
         updateHand("Brick", -1);
@@ -90,6 +181,13 @@ public class Player {
         }
     }
 
+    /**
+     * Builds a city. Updates player's hand. Updates active coordinates map by setting the settlement
+     * flag to false and setting the city flag to true.
+     * 
+     * @param x the x-coordinate of the tile vertex
+     * @param y the y-coordinate of the tile vertex
+     */
     public void buildCity(int x, int y) {
         updateHand("Wheat", -2);
         updateHand("Ore", -3);
