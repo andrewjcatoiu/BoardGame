b104bc0c8f13ad2af94965313517b5497246a876
andrewjcatoiu <andrewjcatoiu26@gmail.com>
2025-01-17 14:08:22 -0600

    DevCard docs

--- Game/DevCard.java
+++ Game/DevCard.java
@@ -1,11 +1,30 @@
+/**
+ * Class responsible for created a development card.
+ * 
+ * @author Andrew Catoiu
+ * @version January 2025
+ */
 public class DevCard {
 
+    /**
+     * The type of development card.
+     */
     public String dev;
 
+    /**
+     * Default constructor.
+     * 
+     * @param dev the type of card (e.g., Knight)
+     */
     public DevCard(String dev) {
         this.dev = dev;
     }
 
+    /**
+     * Getter for the type of card.
+     * 
+     * @return returns a string
+     */
     public String getDev() {
         return this.dev;
     }
