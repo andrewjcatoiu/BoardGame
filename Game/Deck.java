705e20f9de81fa47e0cddd9bb1ae15486af8c1d0
andrewjcatoiu <andrewjcatoiu26@gmail.com>
2025-01-17 13:57:53 -0600

    removed Card class

--- Game/Deck.java
+++ Game/Deck.java
@@ -3,24 +3,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public final class Deck {
-    public static final int MATERIAL_CAPACITY = 19;
-    public ArrayList<Card> mats;
     public ArrayList<DevCard> devs;
 
     public Deck() {
-        this.mats = new ArrayList<>();
         this.devs = new ArrayList<>();
     };
 
     public void initDecks() {
-        String[] materialOptions = {"Wood", "Brick", "Sheep", "Wheat", "Ore"};
-        for (String m : materialOptions) {
-            for (int i = 0; i < MATERIAL_CAPACITY; i++) {
-                Card card = new Card(m);
-                mats.add(card);
-            }
-        }
-
         String[][] devOptions = {{"Knight", "14"}, {"Victory Point", "5"}, {"Year of Plenty", "2"}, {"Monopoly", "2"}, {"Road Building", "2"}};
         for (String[] d : devOptions) {
             String dev = d[0];
@@ -33,25 +22,18 @@ public final class Deck {
     }
 
     public void shuffle() {
-        Collections.shuffle(this.mats);
         Collections.shuffle(this.devs);
     }
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("Resources: [");
-        for (Card card : this.mats) {
-            sb.append(card.getMaterial());
-            sb.append("\t");
-        }
-
-        sb.append("]\nDevelopment Cards: [");
+        StringBuilder sb = new StringBuilder("Development Cards: [");
         for (DevCard devCard : this.devs) {
             sb.append(devCard.getDev());
             sb.append("\t");
         }
-        sb.append("]\n");
 
+        sb.append("]\n");
         return sb.toString();
     }
 }
