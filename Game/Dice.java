9d27c1afeaa8d58537ba0c3b9fb9fe070dc672bb
andrewjcatoiu <andrewjcatoiu26@gmail.com>
2025-01-17 14:13:24 -0600

    Dice docs

--- Game/Dice.java
+++ Game/Dice.java
@@ -2,18 +2,37 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
+/**
+ * Class responsible for dice rolling and game randomness.
+ */
 public class Dice {
 
+    /**
+     * A shared {@code Random} instance used for generating random values.
+     */
     private static final Random random = new Random();
     
+    /**
+     * Default constructor.
+     */
     public Dice() {}
 
+    /**
+     * Rolls the dice.
+     * 
+     * @return returns an integer sum of the two die
+     */
     public int roll() {
         int die1 = random.nextInt(6) + 1;
         int die2 = random.nextInt(6) + 1;
         return die1 + die2;
     }
 
+    /**
+     * Determines the turn rotation of the players. The first person in the list will go first.
+     * 
+     * @return returns a shuffled list of the players
+     */
     public ArrayList<Player> buildPlayerOrder() {
         ArrayList<Player> playerOrder = new ArrayList<>();
         playerOrder.add(new Player("Andrew", "Red"));
